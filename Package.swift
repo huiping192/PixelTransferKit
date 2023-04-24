ab841a8c1a0ecdee3022d61e9ab1fc237ea4650b
Huiping Guo <huiping192@gmail.com>
2023-04-25 00:56:48 +0900

    init 2

--- Package.swift
+++ Package.swift
@@ -5,17 +5,21 @@ import PackageDescription
 
 let package = Package(
     name: "PixelTransferKit",
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13)
+    ],
     products: [
-        // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PixelTransferKit",
             targets: ["PixelTransferKit"]),
     ],
+    dependencies: [
+    ],
     targets: [
-        // Targets are the basic building blocks of a package, defining a module or a test suite.
-        // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PixelTransferKit"),
+            name: "PixelTransferKit",
+            dependencies: []),
         .testTarget(
             name: "PixelTransferKitTests",
             dependencies: ["PixelTransferKit"]),
